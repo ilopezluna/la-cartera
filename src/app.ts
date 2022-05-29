1c926aa4eff86c0d2333f1ca92cdf740c30bf45c
Ignasi López <ignasi.lopez.luna@gmail.com>
2022-05-29 12:52:54 +0200

    Added eslint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 
-export const app = express()
+export const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
